0f57cc64036aa2d0875799059a472e0de1c4560b
Jonas Hoffmann <jonas@wecode.dk>
2022-03-16 13:56:32 +0100

    add tacotron/waveglow model to generate audio samples

---
+++ synthetic_gen/generate.py
@@ -0,0 +1,29 @@
+import torch
+import torchaudio
+import warnings
+warnings.simplefilter('ignore')
+
+tacotron2 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tacotron2', model_math='fp16')
+tacotron2 = tacotron2.to('cuda')
+tacotron2.eval()
+
+waveglow = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_waveglow', model_math='fp16')
+waveglow = waveglow.remove_weightnorm(waveglow)
+waveglow = waveglow.to('cuda')
+waveglow.eval()
+
+text = """
+If you are alone and you need a friend.
+Someone to make you forget your problems.
+Just come along, baby, take my hand.
+"""
+
+utils = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tts_utils')
+sequences, lengths = utils.prepare_input_sequence([text])
+
+with torch.no_grad():
+    mel, _, _ = tacotron2.infer(sequences, lengths)
+    audio = waveglow.infer(mel)
+rate = 22050
+
+torchaudio.save(filepath="audio.wav", src=audio.cpu(), sample_rate=rate)
