8ef720c8798043a29eca028ce4fc85cf2cd1a6d1
Jonas Hoffmann <jonas@triweb.dk>
2022-04-17 19:18:44 +0200

    force tacotron and waveglow update

--- synthetic_gen/generate_parallel.py
+++ synthetic_gen/generate_parallel.py
@@ -30,7 +30,7 @@ class InferModel(object):
         lock = FileLock("tacotron2.lock")
 
         with lock:
-            tacotron2 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tacotron2', model_math='fp16')
+            tacotron2 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_tacotron2', model_math='fp16', force_reload=True)
             tacotron2 = tacotron2.to(torch.device(f"cuda:{self.rank}"))
             tacotron2.decoder.max_decoder_steps = 10000
             tacotron2.eval()
@@ -41,7 +41,7 @@ class InferModel(object):
         lock = FileLock("waveglow.lock")
         with lock:
             torch.cuda.empty_cache()
-            waveglow = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_waveglow', model_math='fp16')
+            waveglow = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_waveglow', model_math='fp16', force_reload=True)
             waveglow = waveglow.remove_weightnorm(waveglow)
             waveglow = waveglow.to(torch.device(f"cuda:{self.rank}"))
             waveglow.eval()
@@ -115,6 +115,8 @@ if __name__ == '__main__':
 
     # Set another local directory to download the models
     #torch.hub.set_dir(TORCH_MODELS_PATH)
+
+
 
     train_clean_100 = torchaudio.datasets.LIBRISPEECH(root=LS_PATH, url=LS_DATASET_TYPE, download=True)
     train_clean_100 = list(map(tuple_to_dict, train_clean_100))
