cd6f4591f11d636592020277ca96022448baba1e
Jonas Hoffmann <jonas@triweb.dk>
2022-04-17 15:18:19 +0200

    adding all training sets

--- upsample_training_audio.sh
+++ upsample_training_audio.sh
@@ -2,8 +2,8 @@
 #BSUB -J authentic_upsampled
 #BSUB -o authentic_upsampled_%J.out
 #BSUB -e authentic_upsampled_%J.err
-#BSUB -n 24
-#BSUB -R "rusage[mem=1G]"
+#BSUB -n 16
+#BSUB -R "rusage[mem=8G]"
 #BSUB -R "span[hosts=1]"
 #BSUB -W 00:10
 #BSUB -u s204071@student.dtu.dk
@@ -19,4 +19,4 @@ module load scipy/1.6.3-python-3.9.6
 # NOTE: needs to have been built with the same SciPy version above!
 source fagprojekt-env/bin/activate
 
-CPU_CORES=24 python3 -m upsampler.upsample
+CPU_CORES=16 python3 -m upsampler.upsample
