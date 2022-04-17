f008bf8378af7f8fc9b8bcad18fdf0595038c750
Joachim Schrøder <joachimandersson0710@gmail.com>
2022-04-17 14:51:37 +0200

    added all authentic sets

--- upsampler/upsample.py
+++ upsampler/upsample.py
@@ -11,9 +11,9 @@ this task using te default parameters from torchaudio.
 
 INPUT_RATE = 16000
 OUTPUT_RATE = 22050
-training_sets = ["test-clean"] #["train-clean-100", "train-clean-360", "train-other-500"]
+sets = ["test-clean", "test-other", "train-clean-100", "train-clean-360", "train-other-500"]
 
-LS_PATH = "{}/data/librispeech".format(os.getcwd()) # skal måske fixes
+LS_PATH = "{}/data/librispeech".format(os.getcwd())
 
 if not os.path.exists(LS_PATH):
     os.makedirs(LS_PATH)
@@ -22,15 +22,15 @@ def audio_upsample(audio, input_rate, output_rate):
     upsample = T.Resample(input_rate, output_rate)
     return upsample(audio)
 
-for training_set in training_sets:
-    OUTPUT_PATH = "{}/data/authentic_speech_upsampled/{}".format(os.getcwd(), training_set)
+for set in sets:
+    OUTPUT_PATH = "{}/data/authentic_speech_upsampled/{}".format(os.getcwd(), set)
 
     if not os.path.exists(OUTPUT_PATH):
         os.makedirs(OUTPUT_PATH)
 
     dataset = torchaudio.datasets.LIBRISPEECH(root=LS_PATH, url=training_set, download=True)
 
-    for sample in tqdm(dataset, desc="Upsampling files for {}".format(training_set)):
+    for sample in tqdm(dataset, desc="Upsampling files for {}".format(set)):
         upsampled = audio_upsample(sample[0], input_rate=INPUT_RATE, output_rate=OUTPUT_RATE)
 
         filename = "s{}_c{}_u{}.wav".format(sample[3], sample[4], sample[5])
