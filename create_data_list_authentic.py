30f18c7b205fee9aad4f3cf30a6a56e9f4dc7d8f
Jonas Hoffmann <jonas@triweb.dk>
2022-04-18 14:31:07 +0200

    update create data list to combined datasets

--- create_data_list_authentic.py
+++ create_data_list_authentic.py
@@ -1,6 +1,7 @@
 import os
 import torchaudio
 from tqdm import tqdm
+from collections import defaultdict
 
 """
 The aim of this file is to create lists of the files to be used in the ASR model.
@@ -12,6 +13,10 @@ have corresponding synthetic, unfiltered counterparts.
 
 datasets = os.listdir('./data/authentic_speech_upsampled/')
 
+combined = {'authentic-train': ['train-clean-100', 'train-clean-360', 'train-other-500']}
+
+combined_datasets = defaultdict(lambda: "")
+
 LIMIT = 18
 SAMPLE_RATE = 22050
 
@@ -21,13 +26,19 @@ for dataset in datasets:
     full_paths = list(
         set(["data/authentic_speech_upsampled/{}/".format(dataset) + filename.replace('.flac', '').replace('.txt', '') for filename
              in files]))
-    filtered_full_paths = []
 
-    for path in tqdm(full_paths):
-        filtered_full_paths.append(path)
+    files_string = "\n".join(full_paths)
 
-    files_string = "\n".join(filtered_full_paths)
+    for cds_name, cds in combined.items():
+        if dataset in cds:
+            combined_datasets[cds_name] += files_string
+
 
     f = open("asr_model/data/librispeech/authentic-{}.txt".format(dataset), "w+")
     f.write(files_string)
+    f.close()
+
+for cds_name, cds in combined_datasets.items():
+    f = open(f"asr_model/data/librispeech/{cds_name}.txt", "w+")
+    f.write(cds)
     f.close()
