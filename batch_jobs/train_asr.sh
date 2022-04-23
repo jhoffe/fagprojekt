72cb60acab5305096e84b1da1eb818b34f4af4d2
Jonas Hoffmann <jonas@triweb.dk>
2022-04-23 18:04:11 +0200

    fix path
    Signed-off-by: Jonas Hoffmann <jonas@triweb.dk>

--- batch_jobs/train_asr.sh
+++ batch_jobs/train_asr.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd work3/s204071/fagprojekt
+cd /work3/s204071/fagprojekt
 bsub < batch_jobs/train_asr_authentic.sh
 bsub < batch_jobs/train_asr_synthetic.sh
 bsub < batch_jobs/train_asr_mixed.sh
