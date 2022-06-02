7626a0d9c8af28210b41bac08a89a131e437ea9d
Jonas Hoffmann <jonas@triweb.dk>
2022-06-02 11:48:35 +0200

    add generator for augmented spectrogram
    Signed-off-by: Jonas Hoffmann <jonas@triweb.dk>

---
+++ asr_model/augmented_spect_gen.py
@@ -0,0 +1,103 @@
+from multiprocessing import Pool
+
+import numpy as np
+import librosa
+import torchaudio
+from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, SpecFrequencyMask, SpecCompose
+import os
+import torch
+import random
+from asr.data import BaseDataset
+from tqdm import tqdm
+
+
+class AugmentedSpectrogramGenerator:
+    def __init__(self, ext='.flac', sample_rate=22050, window_size=0.02, stride=0.01, power_spectrum=True, num_mels=80,
+                 logscale=True, normalize=True, frq_bin=True, output_format='NFHT', should_augment=False):
+
+        assert output_format in ('NFHT', 'NFT', 'TNF'), 'Output format should be NFHT, NFT or TNF.'
+
+        self.ext = ext
+        self.sample_rate = sample_rate
+        self.window_size = window_size
+        self.stride = stride
+        self.power_spectrum = power_spectrum
+        self.num_mels = num_mels
+        self.logscale = logscale
+        self.normalize = normalize
+        self.frq_bin = frq_bin
+        self.output_format = output_format
+        self.should_augment = should_augment
+
+        self.mel_basis = None if num_mels is None else \
+            librosa.filters.mel(sr=sample_rate, n_fft=int(window_size * sample_rate), n_mels=num_mels)
+
+    def augment(self, sample):
+        augmenter = Compose([
+            AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.003, p=1),
+            TimeStretch(min_rate=0.9, max_rate=1.25, p=0.9),
+            PitchShift(min_semitones=-12, max_semitones=12, p=0.98)
+        ], p=0.995)
+
+        return augmenter(samples=sample, sample_rate=self.sample_rate)
+
+    def generate(self, path):
+        path = path[-1]
+        if not path.endswith(self.ext):
+            path = path + self.ext
+
+        pcm, sample_rate = torchaudio.load(path, format="flac")  # wavfile.read(path)
+        pcm = pcm.numpy().reshape(-1)
+
+        assert sample_rate == self.sample_rate, f'Audio file did not have the expected sample rate: {path}'
+        assert len(pcm) > int(self.window_size * self.sample_rate), f'PCM audio has too few samples: {path}'
+        assert np.std(pcm) > 0, f'PCM audio is empty: {path}'
+
+        if self.should_augment:
+            pcm = self.augment(pcm)
+
+        stft = librosa.stft(pcm.astype(np.float64), n_fft=int(self.window_size * self.sample_rate), window='hann',
+                            hop_length=int(self.stride * sample_rate), dtype=np.complex128)
+        spectrogram = np.abs(stft)
+
+        if self.power_spectrum:
+            spectrogram = spectrogram ** 2.
+        if self.num_mels is not None:
+            spectrogram = np.dot(self.mel_basis, spectrogram)
+        if self.logscale:
+            spectrogram = librosa.core.power_to_db(spectrogram, top_db=None)
+        if self.normalize:
+            axis = 1 if self.frq_bin else None
+            mean, std = np.mean(spectrogram, axis=axis), np.std(spectrogram, axis=axis)
+            spectrogram = ((spectrogram.T - mean) / (std + np.finfo(np.float64).eps)).T
+
+        spect = spectrogram.astype(np.float32)
+
+        spec_freq_mask = SpecCompose([SpecFrequencyMask(p=0.98)])
+
+        spect = spec_freq_mask(spect)
+
+        save_path, _ext = os.path.splitext(path)
+
+        np.savez_compressed(f"{save_path}.spect", spect, allow_pickle=True)
+
+        return f"{save_path}.spect"
+
+
+if __name__ == "__main__":
+    SEED = 42
+    torch.manual_seed(SEED)
+    random.seed(SEED)
+    np.random.seed(SEED)
+
+    TRAIN_DATASET_PATH = os.environ['TRAIN_DATASET']
+    CPU_CORES = int(os.environ['CPU_CORES'])
+
+    train_dataset = BaseDataset(source=TRAIN_DATASET_PATH, preprocessor=[], sort_by=0)
+
+    spect_gen = AugmentedSpectrogramGenerator()
+
+    with tqdm(total=len(train_dataset), desc="Generating spectrograms") as pbar:
+        with Pool(int(os.environ["CPU_CORES"])) as p:
+            for _ in p.imap(spect_gen.generate, train_dataset, 12):
+                pbar.update()
