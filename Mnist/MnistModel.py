8838e63662f6bf68c4668ee51064520d55bbee57
Jonas Hoffmann <jonas@triweb.dk>
2022-06-06 10:57:13 +0200

    add wavenet model for mnist
    Signed-off-by: Jonas Hoffmann <jonas@triweb.dk>

--- Mnist/MnistModel.py
+++ Mnist/MnistModel.py
@@ -7,7 +7,6 @@ import torch.nn.functional as F
 from torch.optim.lr_scheduler import CosineAnnealingLR
 import os
 
-
 MnistDataset = torchvision.datasets.MNIST()
 
 data_loader = torch.utils.data.DataLoader(MnistDataset,
@@ -15,22 +14,153 @@ data_loader = torch.utils.data.DataLoader(MnistDataset,
                                           shuffle=True)
 
 
-def CausalConv1d(in_channels, out_channels, kernel_size, dilation=1, **kwargs):
-   pad = (kernel_size - 1) * dilation
-   return nn.Conv1d(in_channels, out_channels, kernel_size, padding=pad, dilation=dilation, **kwargs)
+class WaveNetModel(nn.Module):
+    """
+    A Complete Wavenet Model
+    Args:
+        layers (Int):               Number of layers in each block
+        blocks (Int):               Number of wavenet blocks of this model
+        dilation_channels (Int):    Number of channels for the dilated convolution
+        residual_channels (Int):    Number of channels for the residual connection
+        skip_channels (Int):        Number of channels for the skip connections
+        classes (Int):              Number of possible values each sample can have
+        output_length (Int):        Number of samples that are generated for each input
+        kernel_size (Int):          Size of the dilation kernel
+        dtype:                      Parameter type of this model
+    Shape:
+        - Input: :math:`(N, C_{in}, L_{in})`
+        - Output: :math:`()`
+        L should be the length of the receptive field
+    """
 
-class MnistModel(nn.Module):
-    def __init(self, input_size=28*28, output_size=28*28):
-        super.__init__(self)
-        self.input_size = input_size
+    def __init__(self,
+                 layers=2,
+                 blocks=4,
+                 dilation_channels=16,
+                 residual_channels=8,
+                 skip_channels=4,
+                 classes=32,
+                 output_length=28 * 28,
+                 kernel_size=2,
+                 dtype=torch.FloatTensor,
+                 bias=False,
+                 fast=False):
 
-        self.conv1 = CausalConv1d(in_channels=input_size, out_channels=output_size, kernel_size=3, dilation=1)
+        super(WaveNetModel, self).__init__()
 
-def forward(self, x):
+        self.layers = layers
+        self.blocks = blocks
+        self.dilation_channels = dilation_channels
+        self.residual_channels = residual_channels
+        self.skip_channels = skip_channels
+        self.classes = classes
+        self.kernel_size = kernel_size
+        self.dtype = dtype
+        self.fast = fast
 
-   x = self.conv1(x)
-   x = x[:, :, :-self.conv1.padding[0]]
+        # build model
+        receptive_field = 1
 
-   return x
+        self.filter_convs = nn.ModuleList()
+        self.gate_convs = nn.ModuleList()
+        self.residual_convs = nn.ModuleList()
+        self.skip_convs = nn.ModuleList()
 
+        # 1x1 convolution to create channels
+        self.start_conv = nn.Conv1d(in_channels=self.classes,
+                                    out_channels=residual_channels,
+                                    kernel_size=1,
+                                    bias=bias)
 
+        for b in range(blocks):
+            additional_scope = kernel_size - 1
+            for i in range(layers):
+                # dilated convolutions
+                self.filter_convs.append(nn.Conv1d(in_channels=residual_channels,
+                                                   out_channels=dilation_channels,
+                                                   kernel_size=kernel_size,
+                                                   bias=bias))
+
+                self.gate_convs.append(nn.Conv1d(in_channels=residual_channels,
+                                                 out_channels=dilation_channels,
+                                                 kernel_size=kernel_size,
+                                                 bias=bias))
+
+                # 1x1 convolution for residual connection
+                self.residual_convs.append(nn.Conv1d(in_channels=dilation_channels,
+                                                     out_channels=residual_channels,
+                                                     kernel_size=1,
+                                                     bias=bias))
+
+                # 1x1 convolution for skip connection
+                self.skip_convs.append(nn.Conv1d(in_channels=dilation_channels,
+                                                 out_channels=skip_channels,
+                                                 kernel_size=1,
+                                                 bias=bias))
+
+                receptive_field += additional_scope
+                additional_scope *= 2
+
+        self.end_conv_1 = nn.Conv1d(in_channels=skip_channels,
+                                    out_channels=skip_channels,
+                                    kernel_size=1,
+                                    bias=True)
+
+        self.end_conv_2 = nn.Conv1d(in_channels=skip_channels,
+                                    out_channels=classes,
+                                    kernel_size=1,
+                                    bias=True)
+
+        # self.output_length = 2 ** (layers - 1)
+        self.output_size = output_length
+        self.receptive_field = receptive_field
+        self.input_size = receptive_field + output_length - 1
+
+    def forward(self, input, mode="normal"):
+        if mode == "save":
+            self.inputs = [None] * (self.blocks * self.layers)
+
+        x = self.start_conv(input)
+        skip = 0
+
+        # WaveNet layers
+        for i in range(self.blocks * self.layers):
+
+            #            |----------------------------------------|     *residual*
+            #            |                                        |
+            #            |    |-- conv -- tanh --|                |
+            # -> dilate -|----|                  * ----|-- 1x1 -- + -->	*input*
+            #                 |-- conv -- sigm --|     |
+            #                                         1x1
+            #                                          |
+            # ---------------------------------------> + ------------->	*skip*
+
+            if mode == "save":
+                self.inputs[i] = x[:, :, 0 + 1:]
+            elif mode == "step":
+                self.inputs[i] = torch.cat([self.inputs[i][:, :, 1:], x], dim=2)
+                x = self.inputs[i]
+
+            # dilated convolution
+            residual = x
+
+            filter = self.filter_convs[i](x)
+            filter = torch.tanh(filter)
+            gate = self.gate_convs[i](x)
+            gate = torch.sigmoid(gate)
+            x = filter * gate
+
+            # parametrized skip connection
+            s = self.skip_convs[i](x)
+            if skip is not 0:
+                skip = skip[:, :, -s.size(2):]
+            skip = s + skip
+
+            x = self.residual_convs[i](x)
+            x = x + residual[:, :, 0:]
+
+        x = torch.relu(skip)
+        x = torch.relu(self.end_conv_1(x))
+        x = self.end_conv_2(x)
+
+        return x
