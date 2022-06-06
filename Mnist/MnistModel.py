fe9b94597e320f4cd4c620fb7a51a82ba6eae4a6
Jonas Hoffmann <jonas@triweb.dk>
2022-06-06 14:01:44 +0200

    fix end convs
    Signed-off-by: Jonas Hoffmann <jonas@triweb.dk>

--- Mnist/MnistModel.py
+++ Mnist/MnistModel.py
@@ -40,17 +40,21 @@ class CausalConv1d(nn.Conv1d):
 
 
 class CausalModel(nn.Module):
-    def __init__(self, input_size=28 * 28, output_size=28 * 28, layers=3):
+    def __init__(self, input_size=28 * 28, output_size=28 * 28, layers=3, kernel_size=2, bias=True):
         self.input_size = input_size
         self.output_size = output_size
         self.layers = layers
+        self.kernel_size = kernel_size
 
         super(CausalModel, self).__init__()
 
-        self.causal_conv = CausalConv1d(in_channels=input_size, out_channels=output_size, kernel_size=1)
+        self.causal_conv = CausalConv1d(in_channels=input_size, out_channels=output_size, kernel_size=kernel_size, bias=bias)
+        self.end_conv1d = nn.Conv1d(in_channels=input_size, out_channels=output_size, kernel_size=1, bias=True)
 
     def forward(self, x):
         for _ in range(self.layers):
-            self.causal_conv.forward(x)
+            x = F.relu(self.causal_conv.forward(x))
+
+        x = F.relu(self.end_conv1d.forward(x))
 
         return x
