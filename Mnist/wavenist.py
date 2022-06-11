7a103a66d1d56e37073a1dab0d860a96be9f5678
Jonas Hoffmann <jonas@triweb.dk>
2022-06-11 12:24:09 +0200

    add wavenist model
    Signed-off-by: Jonas Hoffmann <jonas@triweb.dk>

---
+++ Mnist/wavenist.py
@@ -0,0 +1,106 @@
+import pytorch_lightning as pl
+import torch
+import torch.nn.functional as F
+from pytorch_lightning.loggers import CSVLogger
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+from psutil import cpu_count
+
+
+class CausalConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dilation, A=False, **kwargs):
+        super(CausalConv1d, self).__init__()
+
+        self.kernel_size = kernel_size
+        self.dilation = dilation
+        self.A = A
+        self.padding = (kernel_size - 1) * dilation + A * 1
+
+        self.conv1d = nn.Conv1d(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=dilation,
+                                **kwargs)
+
+    def forward(self, x):
+        x = F.pad(x, (self.padding, 0))
+        conv_out = self.conv1d(x)
+        if self.A:
+            return conv_out[:, :, :-1]
+        else:
+            return conv_out
+
+
+class WaveNIST(pl.LightningModule):
+    def __init__(self, layers=3, hidden=256, kernel_size=3, output_classes=256):
+        super(WaveNIST, self).__init__()
+        self.output_classes = output_classes
+
+        self.first_conv = CausalConv1d(in_channels=1, out_channels=hidden, dilation=1, kernel_size=kernel_size, A=True,
+                                       bias=True)
+        self.hidden_convs = nn.ModuleList([CausalConv1d(in_channels=hidden, out_channels=hidden, dilation=1,
+                                                        kernel_size=kernel_size, A=False, bias=True)] * layers)
+        self.end_conv = CausalConv1d(in_channels=hidden, out_channels=output_classes, dilation=1,
+                                     kernel_size=kernel_size, A=False, bias=True)
+
+        self.activation = nn.ReLU()
+        self.final_activation = nn.Sigmoid()
+        self.loss_fn = nn.NLLLoss()
+
+    def forward(self, x):
+        x = self.first_conv(x)
+
+        for h in self.hidden_convs:
+            x = self.activation(h(x))
+
+        x = self.end_conv(x)
+
+        return torch.log_softmax(x, 1)
+
+    def generate(self, batch_size, output_length):
+        generated = torch.zeros((batch_size, output_length))
+
+        for t in range(output_length):
+            p = self.forward(generated)
+            y = torch.multinomial(p[:, t, :], num_samples=1)
+            generated[:, t] = y[:, 0]
+
+        return generated
+
+    def discretize_input(self, x):
+        return torch.bucketize(x, torch.Tensor([1 / self.output_classes * i for i in range(self.output_classes - 1)])).squeeze()
+
+    def training_step(self, batch, batch_idx):
+        x, _ = batch
+        y = self.discretize_input(x)
+        p = self.forward(x)
+        loss = self.loss_fn(p, y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, _ = batch
+        y = self.discretize_input(x)
+        p = self.forward(x)
+        val_loss = self.loss_fn(p, y)
+        self.log("val_loss", val_loss)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+classes = 256
+input_transforms = transforms.Compose([transforms.ToTensor(), transforms.Lambda(lambda x: x.flatten(1))])
+
+train_set = datasets.MNIST(root="MNIST", download=True, train=True, transform=input_transforms)
+val_set = datasets.MNIST(root="MNIST", download=True, train=False, transform=input_transforms)
+
+train_loader = DataLoader(train_set, batch_size=32, num_workers=cpu_count())
+val_loader = DataLoader(val_set, batch_size=32, num_workers=cpu_count())
+
+pl.seed_everything(42, workers=True)
+
+model = WaveNIST()
+trainer = pl.Trainer(accelerator="gpu" if torch.cuda.is_available() else "cpu",
+                     devices=-1 if torch.cuda.is_available() else None, max_epochs=1000,
+                     logger=CSVLogger(save_dir="logs"),
+                     default_root_dir="models/")
+trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
---
+++ batch_jobs/train_wavenist.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#BSUB -J train_wavenist
+#BSUB -o batch_jobs/logs/train_wavenist_%J.out
+#BSUB -e batch_jobs/logs/train_wavenist_%J.err
+#BSUB -q gpua100
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -n 16
+#BSUB -R "rusage[mem=4G]"
+#BSUB -R "span[hosts=1]"
+#BSUB -W 06:00
+#BSUB -u s204071@student.dtu.dk
+#BSUB -B
+#BSUB -N
+# end of BSUB options
+
+cd /work3/s204071/fagprojekt
+
+# load a scipy module
+# replace VERSION and uncomment
+module load scipy/1.6.3-python-3.9.6
+
+# load CUDA (for GPU support)
+module load cuda/11.3
+
+# activate the virtual environment
+# NOTE: needs to have been built with the same SciPy version above!
+source fagprojekt-env/bin/activate
+
+python3 Mnist/mnist_wavenet.py
