09558899c5b95307189a246d4883e52add98fa3b
Jonas Hoffmann <jonas@triweb.dk>
2022-06-13 11:33:17 +0200

    add model path
    Signed-off-by: Jonas Hoffmann <jonas@triweb.dk>

--- batch_jobs/finetune_asr_authentic.sh
+++ batch_jobs/finetune_asr_authentic.sh
@@ -37,5 +37,6 @@ export CPU_CORES=16
 export LOAD_SPECTROGRAMS=1
 export WANDB_API_KEY="5403fe6e39e261a91fd0a604a0ea7e22c75927cf"
 export WANDB_RUN_GROUP="finetune"
+export PRETRAINED_MODEL_PATH="asr_model/models/asr_model_authentic.pt"
 
 python3 asr_model/Finetune.py
