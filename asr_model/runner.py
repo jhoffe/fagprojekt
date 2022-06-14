797174495e31cf4214a5c1314e7aff71376382fa
Joachim Schrøder <joachimandersson0710@gmail.com>
2022-06-14 10:52:02 +0200

    apply cosine annealing

--- asr_model/runner.py
+++ asr_model/runner.py
@@ -29,8 +29,7 @@ class Runner:
         self.val_loader = val_loader
         self.loss = nn.CTCLoss(reduction='sum').cuda()
         self.optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-        #self.lr_scheduler = CosineAnnealingLR(self.optimizer, T_max=100, eta_min = 5e-5)
-        self.lr_scheduler = CosineAnnealingWarmRestarts(self.optimizer, T_0=100000, eta_min=5e-5)
+        self.lr_scheduler = CosineAnnealingLR(self.optimizer, T_max=75000, eta_min = 5e-5)
         #self.lr_scheduler = ReduceLROnPlateau(self.optimizer, patience=1, min_lr=3e-4, factor=0.5) # didnt work as intended
         self.validate_every = validate_every
         self.text_preprocessor = TextPreprocessor()
@@ -174,8 +173,8 @@ class Runner:
 
                 self.train_logger.reset()
                 self.model.train()
-                if i >= 100000:
-                    self.lr_scheduler.step(epoch=i/iters)
+                if i >= 75000:
+                    self.lr_scheduler.step(epoch=i)
                     wandb.log({
                         "last_learning_rate": self.lr_scheduler.get_last_lr()[-1]
                     })
